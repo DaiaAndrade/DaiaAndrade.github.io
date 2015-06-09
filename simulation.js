e2e32e4bc2ffde231ad10399ba8da7e96cc52406
DaiaAndrade <daiafocking@gmail.com>
2015-06-09 02:19:59 -0400

    js things

--- simulation.js
+++ simulation.js
@@ -1,36 +1,56 @@
-var userChoice = prompt("Rock, Scisors ou Paper?");
-var computerChoice = Math.random();
-if (computerChoice < 0.34) {
-	computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
-	computerChoice = "paper";
-} else {
-	computerChoice = "scisors";
-} console.log("Computer: " + computerChoice);
-
-compare(userChoice,computerChoice);
-var compare = function(choice1,choice2){
-	if (choice1 === choice2){
-		return "It's a tie!";}
-	else if(choice1 == "rock"){
-		if(choice2=="scisors"){
-			return "Rock wins";}
-		else{
-			return "Paper wins";}
+var main = function(userChoice) {
+	
+	var computerChoice = Math.random();
+	if (computerChoice < 0.34) {
+		computerChoice = "rock";
 	}
-	else if(choice == "paper"){
-		if (coice2 == "rock") {
+	else if(computerChoice >= 0.34 && computerChoice <= 0.67) {
+		computerChoice = "paper";
+	}
+	else {
+		computerChoice = "scisors";
+	}
+	if (userChoice == 1) {
+		userChoice = "rock";
+	}
+	else if(userChoice == 2) {
+		userChoice = "paper";
+	}
+	else {
+		userChoice = "scisors";
+	}
+	document.getElementById("yourChoice").innerHTML = "Your Choice: " + userChoice;
+	document.getElementById("computer").innerHTML = "Computer Choice: " + computerChoice;
+	var result = compare(userChoice,computerChoice);
+	document.getElementById("result").innerHTML = result;
+}
+
+var compare = function(choice1,choice2){
+	if (choice1 == choice2){
+		return "It is a tie!";
+	}
+	else if(choice1 == "rock"){
+		if(choice2 == "scisors"){
+			return "Rock wins";
+		}
+		else{
 			return "Paper wins";
-		};
+		}
+	}
+	else if(choice1 == "paper"){
+		if (choice2 == "rock") {
+			return "Paper wins";
+		}
 		else{
 			return "Scisors wins";
 		}
 	}
 	else{
 		if(choice2 == "rock"){
-			return "Rock wins";}
+			return "Rock wins";
 		}
 		else{
 			return "Scisors wins";
 		}
 	}
+}
