0b0d53bcd02e40e525556741852b86b53fb873ba
DaiaAndrade <daiafocking@gmail.com>
2015-06-20 06:10:06 -0400

    PC 24

---
+++ main.js
@@ -0,0 +1,24 @@
+var simModel = new pipit.CapiAdapter.CapiModel({
+	answer: false,
+	studentResponse: "5"
+});
+
+
+pipit.CapiAdapter.expose('answer', simModel);
+pipit.Controller.notifyOnReady();
+
+var answered == 1;
+var questionNumber == 1;
+
+function questions() {  
+	/*var answer1 = document.question.question1;  
+	if(option == 1)  {  
+		checkAnswer1(answer1);
+	} */ 
+	if (anwsered == questionNumber){
+		simModel.set('answer', true);
+		pipit.CapiAdapter.expose('answer', simModel);
+
+	}
+	pipit.Controller.notifyOnReady();	
+}
