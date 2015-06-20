468972870b9adf646222c96de0ba22af639d3d32
DaiaAndrade <daiafocking@gmail.com>
2015-06-20 04:08:46 -0400

    pipit try outs
    new one, to compose the js that will have the answers

---
+++ research/appTryOut/main.js
@@ -0,0 +1,20 @@
+var answered == 1;
+var questionNumber == 1;
+
+var simModel = new pipit.CapiAdapter.CapiModel({
+	done: false
+});
+pipit.CapiAdapter.expose('demoMode', simModel);
+
+function questions() {  
+	var answer1 = document.question.question1;  
+	if(option == 1)  {  
+		checkAnswer1(answer1);
+	}  
+	if (anwsered == questionNumber){
+		simModel.set('done', true);
+		pipit.CapiAdapter.expose('done', simModel);
+
+	}
+}
+pipit.Controller.notifyOnReady();
